9dc73caac1061d6c2135c78666693b9ab8724589
Edwin Puentes <edwin.puentes.florez@gmail.com>
2020-05-10 23:18:46 -0500

    Update

--- index.php
+++ index.php
@@ -554,7 +554,7 @@
 						<p>Esquema multiplataforma para que su pagina llegue a mas personas.</p>
 						<p>Paginas completamente interactivas, seguras, rapidas y eficaces.</p>
 						<p>Conectividad con bases de datos SQL o No SQL.</p>
-						<button type="submit" name="sendmail" class="btn">Send Message</button>
+						<!--button type="submit" name="sendmail" class="btn">Send Message</button-->
 						
 					</div>
 					<div class="modal-footer">
@@ -602,7 +602,7 @@
 						<p>Remanufacturacion de software.</p>
 						<p>Mantenimiento de aplicaciones.</p>
 						<p>Migracion de aplicaciones.</p>
-						<p>M.</p>
+						
 					</div>
 					<div class="modal-footer">
 						<button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
@@ -668,12 +668,10 @@
 					<div class="modal-body">
 						<img src="images/test.mkv" alt="img01" class="img-responsive" />
 						<div class="modal-works"><span>Industrias</span><span>Hogares</span></div>
-						<p>Consultoria y aplicacion de medidas para la seguridad informatica:</p>
-						<p>Pen Testing </p>
-						<p>Implementacion de politicas de seguridad funcionales </p>
-						<p>Honey Pots </p>
-						<p>Firewalls </p>
-						
+						<p>Diseño de sistemas de control y analisis de:</p>
+						<p>Servicios Publicos</p>
+						<p>Electrodomesticos, Redes o Maquinaria Industrial </p>
+						<p>Dispositivos de seguridad o supervisión electronica </p>					
 					</div>
 					<div class="modal-footer">
 						<button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
