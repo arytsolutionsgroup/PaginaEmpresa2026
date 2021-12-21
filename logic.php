82703cf836ade7c468eb53393f632b7fa5e440b9
Edwin <46654349+edwinpuentes@users.noreply.github.com>
2021-12-20 22:40:25 -0500

    test

--- logic.php
+++ logic.php
@@ -11,6 +11,7 @@
 <?php 
 			// Import PHPMailer classes into the global namespace
 			// These must be at the top of your script, not inside a function
+			
 			use PHPMailer\PHPMailer\PHPMailer;
 			use PHPMailer\PHPMailer\SMTP;
 			use PHPMailer\PHPMailer\Exception;
